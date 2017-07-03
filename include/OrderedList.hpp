f2250d85fcd96694e2a15cc3e4c0273b20e38ed6
Douglas <douglasrizzom@gmail.com>
2017-07-03 17:51:46 -0300

    created indexOf and contains

--- include/OrderedList.hpp
+++ include/OrderedList.hpp
@@ -15,61 +15,81 @@
 //! \tparam T The type of object the data structure will contain
 template<class T>
 class OrderedList : public LinkedList<T> {
-private:
-    function<int(T, T)> compare;
+ private:
+  function<int(T, T)> compare;
 
-public:
-    string getName() { return "Ordered List"; }
+ public:
+  string getName() { return "Ordered List"; }
 
-    /*!
-       \brief Creates the structure
-       \param compareFunc a C++ 11 compliant function parameter that allows comparation between template objects
-    */
-    explicit OrderedList(std::function<int(T, T)> compareFunc) {
-        compare = compareFunc;
+  /*!
+     \brief Creates the structure
+     \param compareFunc a C++ 11 compliant function parameter that allows comparation between template objects
+  */
+  explicit OrderedList(std::function<int(T, T)> compareFunc) {
+    compare = compareFunc;
+  }
+
+  //! create the structure and populate it with the data from the array
+  //! \param data an array with data with which the structure will be initialized
+  //! \param compareFunc a C++ 11 compliant function parameter that allows comparation between template objects
+  explicit OrderedList(const T data[], std::function<int(T, T)> compareFunc) {
+    compare = compareFunc;
+    for (int i = 0; i <= (sizeof(data)/sizeof(data[0])); i++) {
+      // use this class implementation of insert() to ensure order
+      insert(data[i]);
+    }
+  }
+
+  //! Insert an element at its position on the list
+  //! \param val the value to be inserted
+  void insert(T val) {
+    if (LinkedList<T>::isEmpty()) {
+      LinkedList<T>::insert(val);
+    } else {
+      Node<T> *tmp = LinkedList<T>::getFirst();
+      int index = 0;
+      while (tmp!=NULL && compare(val, tmp->getValue()) < 0) {
+        index++;
+        tmp = tmp->getNext();
+      }
+      LinkedList<T>::insert(val, index);
+    }
+  }
+
+  void insert(T val, int index) {
+    insert(val);
+  }
+
+  //! Finds an element in the ordered list and returns its index.
+  //! \param val the element to be found
+  //! \return The index of the first element where compareFunc(val, element) == 0
+  int indexOf(T val) {
+    Node<T> *tmp = LinkedList<T>::getFirst();
+    int index = 0;
+    while (tmp!=NULL && compare(val, tmp->getValue()) >= 0) {
+      if (compare(val, tmp->getValue()) == 0)
+        return index;
+      index++;
+      tmp = tmp->getNext();
     }
 
-    //! create the structure and populate it with the data from the array
-    //! \param data an array with data with which the structure will be initialized
-    //! \param compareFunc a C++ 11 compliant function parameter that allows comparation between template objects
-    explicit OrderedList(const T data[], std::function<int(T, T)> compareFunc) {
-        compare = compareFunc;
-        for (int i = 0; i <= (sizeof(data) / sizeof(data[0])); i++) {
-            // use this class implementation of insert() to ensure order
-            insert(data[i]);
-        }
-    }
+    return -1;
+  }
 
-    //! Insert an element at its position on the list
-    //! \param val the value to be inserted
-    void insert(T val) {
-        if (LinkedList<T>::isEmpty()) {
-            LinkedList<T>::insert(val);
-        }
-        else {
-            Node<T> *tmp = LinkedList<T>::getFirst();
-            int index = 0;
-            while (tmp != NULL) {
-                if(compare(val, tmp->getValue()) >= 0)
-                    break;
-                index++;
-                tmp = tmp->getNext();
-            }
-            LinkedList<T>::insert(val, index);
-        }
-    }
+  //! Answers whether the ordered list contains an enemy.
+  //! \param val the element to be found
+  //! \return true if there is at least one element where compareFunc(val, element) == 0, otherwise false
+  bool contains(T val) {
+    return indexOf(val)!=-1;
+  }
 
-    void insert(T val, int index) {
-        insert(val);
-    }
+  T remove(int index) {
+    return LinkedList<T>::remove(index);
+  }
 
-    T remove(int index) {
-        return LinkedList<T>::remove(index);
-    }
-
-    T get(int index) {
-        return LinkedList<T>::get(index);
-    }
+  T get(int index) {
+    return LinkedList<T>::get(index);
+  }
 };
 
 #endif
