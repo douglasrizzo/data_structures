ffdf15661af43fea6a5d395e81cedf2df0c7cb47
Douglas <douglasrizzom@gmail.com>
2017-07-03 18:43:15 -0300

    updated docs after creating new methods for OrderedList

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['compare',['compare',['../classOrderedList.html#a40320b7b6513b1f48270358cceb66502',1,'OrderedList::compare()'],['../main_8cpp.html#a26d99f4a70c34b9140e3cc22221b1998',1,'compare():&#160;main.cpp']]],
+  ['contains',['contains',['../classOrderedList.html#a7ae22e189c25baa8748b5bf3de4520f8',1,'OrderedList']]],
   ['count',['count',['../classStaticQueue.html#a01ae57feaa61b79fc7a7e57531ad9fa3',1,'StaticQueue']]],
   ['current',['current',['../classIterator.html#a0b3c374b7383047f3d7d19492876e738',1,'Iterator']]]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['compare',['compare',['../main_8cpp.html#a26d99f4a70c34b9140e3cc22221b1998',1,'main.cpp']]],
+  ['contains',['contains',['../classOrderedList.html#a7ae22e189c25baa8748b5bf3de4520f8',1,'OrderedList']]],
   ['current',['current',['../classIterator.html#a0b3c374b7383047f3d7d19492876e738',1,'Iterator']]]
 ];
